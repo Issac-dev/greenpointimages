9788abaa55ea237bf6b490ed854393d576e38235
Issac-dev <71912176+Issac-dev@users.noreply.github.com>
2021-10-21 21:09:55 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,10 @@
+$(document).ready(function () {
+    $("#part-1").fadeIn(4000);
+    $("#part-2").fadeIn(7000);
+    $("#part-3").fadeIn(10000);
+    $("#part-4").fadeIn(13000);
+    $("#part-5").fadeIn(16000);
+    $("#part-6").fadeIn(1700);
+
+}
+);
